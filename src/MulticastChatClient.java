876e894145a3a6aed957a22abc3f762ef89200fc
Tia Hedlund <tia.hedlund@elev.ga.ntig.se>
2025-05-07 12:41:24 +0200

    Bytte till mitt portnumber som jag valde

--- src/MulticastChatClient.java
+++ src/MulticastChatClient.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+
+import java.net.*;
+import java.io.*;
 
 public class MulticastChatClient {
+
+    public static void main(String args[]) throws Exception {
+        int portnumber = 53004;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        MulticastSocket chatMulticastSocket = new MulticastSocket(portnumber);
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+        chatMulticastSocket.joinGroup(group);
+
+        String msg = "";
+        System.out.println("Type a message for the server:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        msg = br.readLine();
+
+        DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+        chatMulticastSocket.send(data);
+
+        chatMulticastSocket.close();
+    }
 }
+
